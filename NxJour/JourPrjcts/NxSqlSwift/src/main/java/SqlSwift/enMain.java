93ed90ca0d504044f3f97a969e7d7bd68df8d8f8
root <christyler@126.com>
2016-12-08 22:18:12 -0800

    make changes

---
+++ NxJour/JourPrjcts/NxSqlSwift/src/main/java/SqlSwift/enMain.java
@@ -0,0 +1,11 @@
+package SqlSwift;
+
+/**
+ * Created by root on 12/8/16.
+ */
+public class enMain {
+
+    public static void main(String [] args){
+        System.out.println("App started!");
+    }
+}
