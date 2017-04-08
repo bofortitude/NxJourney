b2b8153fbe1d02b968cdd69777e3fe1e1982e3a7
Bo Fei <bofei@fortinet-us.com>
2017-04-08 11:34:47 -0700

    make changes

---
+++ FortiadcLab/fortiadc_lab_server/fortiadc_lab_server/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fortiadc_lab_server project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortiadc_lab_server.settings")
+
+application = get_wsgi_application()
